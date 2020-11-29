8116c0b2e5138b4fdc859a899137da24aeea819f
sg <sylvain.guillard@qrious.co.nz>
2020-11-29 20:02:52 +1300

    test second

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Testß</div>;
+  return <div className={styles.container}>This is my second test</div>;
 }
